66c238c9310d7a83b2b697e25073e4aa4a66ac57
Karl <karlfredriechgetuya@gmail.com>
2024-04-28 02:14:57 +0800

    Website layout

---
+++ layout/navbar.php
@@ -0,0 +1,15 @@
+<div class="container-fluid px-5 py-3 shadow" style="z-index: 1;">
+    <div class="container d-flex justify-content-between align-items-center">
+        <div class="logo">
+            <a href="./home.php"><img src="./assets/logo.jpg" alt="Logo" class="logo" style="width: 50px"></a>
+        </div>
+        <nav class="navbar navbar-expand-sm">
+            <ul class="navbar-nav list-unstyled d-flex">
+                <li class="nav-item"><a href="home.php" class="nav-link text-decoration-none text-dark">Home</a></li>
+                <li class="nav-item"><a href="about.php" class="nav-link text-decoration-none text-dark">About</a></li>
+                <li class="nav-item"><a href="services.php" class="nav-link text-decoration-none text-dark">Services</a></li>
+                <li class="nav-item"><a href="contact.php" class="nav-link text-decoration-none text-dark">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
